34f5958baaa9b6bfc664eeb54e8a89ae86ae2e89
mgorzelsky <m.d.gorzelsky@gmail.com>
2019-11-21 10:34:47 -0800

    minor changes in an attempt to make it run in browsers

--- server.js
+++
@@ -1,8 +0,0 @@
-//node server.js
-//You can now go to http://localhost:8080/index.html
-
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
