1b5a4fd45c8f782af857f322727a75c65dbc89a0
mgorzelsky <m.d.gorzelsky@gmail.com>
2019-11-20 16:05:20 -0800

    added server.js to run a local node server for testing outside of codesandbox.io

---
+++ server.js
@@ -0,0 +1,8 @@
+//node server.js
+//You can now go to http://localhost:8080/index.html
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
