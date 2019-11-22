57b0c96ed6ffc3200b66e0d6b7c0b70571d27a0a
mgorzelsky <m.d.gorzelsky@gmail.com>
2019-11-22 13:29:09 -0800

    ships visually rotate to follow the player

--- src/enemyfighter.js
+++ src/enemyfighter.js
@@ -5,12 +5,19 @@ export default class EnemyFighter {
     this.game = game;
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
-    this.width = 27;
-    this.height = 39;
+
+    this.angle = 0;
+    this.width = 39;
+    this.height = 27;
     this.position = {
       x: Math.floor(Math.random() * game.gameWidth),
       y: Math.floor(Math.random() * (game.gameHeight / 2))
     };
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+
     this.maxSpeed = 3;
     this.speed = {
       x: 0,
@@ -34,13 +41,34 @@ export default class EnemyFighter {
   }
 
   TrackPlayer() {
-    this.fireVector.x = this.game.ship.center.x - this.projectileOriginPoint.x;
-    this.fireVector.y = this.game.ship.center.y - this.projectileOriginPoint.y;
+    this.fireVector.x = this.game.ship.center.x - this.projectileOriginPoint.x; // a
+    this.fireVector.y = this.game.ship.center.y - this.projectileOriginPoint.y; // b
 
-    let magnitude = Math.sqrt(Math.pow(this.fireVector.x, 2) + Math.pow(this.fireVector.y, 2));
+    let magnitude = Math.sqrt(Math.pow(this.fireVector.x, 2) + Math.pow(this.fireVector.y, 2)); // c
 
+    // angle B
+    //Calculate angle from enemy fighter to player in radians
+    this.angle = this.CalculateOrientation(magnitude);
+    //this.angle = (Math.acos((magnitude ** 2 + this.fireVector.x ** 2 - this.fireVector.y ** 2) / (2 * magnitude * this.fireVector.x)));
+
+    //Normalized values so that projectiles move the same speed regardless of distance to the target.
     this.fireVector.x = (this.fireVector.x / magnitude) * this.projectileSpeed;
     this.fireVector.y = (this.fireVector.y / magnitude) * this.projectileSpeed;
+  }
+
+  CalculateOrientation(c) {
+    //TOP LEFT
+    if (this.game.ship.center.x < this.center.x && this.game.ship.center.y < this.center.y) {
+      return Math.acos((c ** 2 + this.fireVector.x ** 2 - this.fireVector.y ** 2) / Math.abs((2 * c * this.fireVector.x)));
+    }
+    //TOP RIGHT
+    else if(this.game.ship.center.x > this.center.x && this.game.ship.center.y < this.center.y) {
+      return Math.acos((c ** 2 + this.fireVector.x ** 2 - this.fireVector.y ** 2) / -(2 * c * this.fireVector.x));
+    }
+    //positive y
+    else {
+      return Math.acos((c ** 2 + this.fireVector.x ** 2 - this.fireVector.y ** 2) / (2 * c * this.fireVector.x));
+    }
   }
 
   Fire() {
@@ -48,19 +76,36 @@ export default class EnemyFighter {
   }
 
   Draw(ctx) {
-    ctx.drawImage(
-      this.image,
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    );
+    ctx.translate(this.center.x, this.center.y);
+    //TOP LEFT
+    if (this.game.ship.center.x < this.center.x && this.game.ship.center.y < this.center.y) {
+      ctx.rotate(this.angle + Math.PI);
+      ctx.drawImage(this.image, -(this.width / 2), -(this.height / 2));
+      ctx.rotate(-(this.angle + Math.PI));
+    }
+    //TOP RIGHT
+    else if (this.game.ship.center.x > this.center.x && this.game.ship.center.y < this.center.y) {
+      ctx.rotate(this.angle + Math.PI);
+      ctx.drawImage(this.image, -(this.width / 2), -(this.height / 2));
+      ctx.rotate(-(this.angle + Math.PI));
+    }
+    //BOTTOM 
+    else {
+      ctx.rotate(this.angle);
+      ctx.drawImage(this.image, -(this.width / 2), -(this.height / 2));
+      ctx.rotate(-this.angle);
+    }
+    ctx.translate(-this.center.x, -this.center.y);
   }
 
   Update(deltaTime) {
     this.TrackPlayer();
     this.position.x += this.speed.x / deltaTime;
     this.position.y += this.speed.y / deltaTime;
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
 
     if (this.position.x < 0) this.position.x = 0;
     if (this.position.x + this.width > this.gameWidth)
